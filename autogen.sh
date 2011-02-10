b27e98cf271c290557a99fb02f1dffc83329d320
Niklas Karlsson <nkarlsson@physics.umn.edu>
2011-02-10 11:03:14 -0600

    Modified this to create the m4 directory for the macros.

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,10 @@ if [ ! -d config ]; then
 		echo "mkdir config"
 		mkdir config
 fi
+if [ ! -d m4 ]; then
+		echo "mkdir m4"
+		mkdir m4
+fi
 autoreconf --force --install -I config && exit 0
 
 exit 1

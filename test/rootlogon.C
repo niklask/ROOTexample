5f18af5a99387b528b33fdf79cb8460c7d5722d2
Niklas Karlsson <nkarlsson@physics.umn.edu>
2011-02-14 14:00:26 -0600

    Changed the default -I path to use the HOME variable

--- test/rootlogon.C
+++ test/rootlogon.C
@@ -1,9 +1,13 @@
 {
 
+// This rootlogon.C will load the test macro. It assumes you have set
+// the LD_LIBRARY_PATH correctly and that the header file is in
+// ${HOME}/usr/include. If not, change that line below.
+
 cout << "Loading the shared library..." << endl;
 gSystem->Load("libROOTobject.so");
 
-gSystem->AddIncludePath(" -I/home/nkarlsson/usr/include/");
+gSystem->AddIncludePath(" -I${HOME}/usr/include/");
 
 gROOT->LoadMacro("macro.C+");
 
